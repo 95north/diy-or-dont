10179cd649de13eb08b612bc35ff4a8795fb28d7
Tori Sigler <mssigler@gmail.com>
2019-08-27 18:38:22 -0400

    Api vs much better

--- app/controllers/user_projects_controller.rb
+++ app/controllers/user_projects_controller.rb
@@ -4,27 +4,38 @@ class UserProjectsController < ApplicationController
 
     def one_user_projects
         user = User.find(params["id"])
-        user_projects_array=[user]
-        # user.projects
-        @ups = user.user_projects   # Works! 
-        #user_projects = user.projects
+
+
         #render json: {projects: user_projects}
         # render json: user, root: false  #NOPE 
         #render json: user, user_project_serializer: UserProjectSerializer  # Just renders user!
         # render json: {user_project_serializer: UserProjectSerializer}  # Just renders user!
         # render json: user, serializer: UserProjectSerializer, root: user_project  # Just renders user!
         # FAST JSON API 1 -- hash = UserProjectSerializer.new(user).serializable_hash
-        @ups.each do |uproj| 
-            u = UserProjectSerializer.new(uproj).serializable_hash
-            p = Project.find( u[:data][:attributes][:project_id] )
-            debugger
-                u["proj"]= p
-                user_projects_array.push(u)
-                
-            # debugger
-        end
 
-        render json: user_projects_array
+        # FAST JSON API 2 - works, but ugly
+        # user_projects_array=[user]
+        @ups = user.user_projects   # Works! 
+        # @ups.each do |uproj| 
+        #     u = UserProjectSerializer.new(uproj).serializable_hash
+        #     p = Project.find( u[:data][:attributes][:project_id] )
+        #     debugger
+        #         u["proj"]= p
+        #         user_projects_array.push(u)
+                
+        #     # debugger
+        # end
+        output = []
+
+        @ups.each do |uproj|
+            p = Project.find( uproj.project_id )
+            output.push([user.username, user.location, uproj, p])
+            # debugger
+        end   
+        # render json: user_projects_array
+        # render json:{user_project: @ups, user: @ups.user, project: @ups.project}  there are SEVERAL UserProjects! This works for one! 
+        render json: output
+        
     end
   
 
