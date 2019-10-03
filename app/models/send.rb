a4aa574bef6e70a5baed0b6bd9a3a2c3d8b02479
Tori Sigler <mssigler@gmail.com>
2019-10-03 11:13:53 -0400

    send model  nexmo constants

--- app/models/send.rb
+++ app/models/send.rb
@@ -1,9 +1,21 @@
 require 'dotenv/load'
 require 'nexmo'
 
-NEXMO_API_KEY = ENV['NEXMO_API_KEY']
-NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
-TO_NUMBER = ENV['TO_NUMBER']
+# NEXMO_API_KEY = ENV['NEXMO_API_KEY']
+# NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
+# TO_NUMBER = ENV['TO_NUMBER']
+
+if !defined?(NEXMO_API_KEY)
+  NEXMO_API_KEY = ENV['NEXMO_API_KEY']
+end
+
+if !defined?(NEXMO_API_SECRET)
+  NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
+end 
+
+if !defined?(TO_NUMBER) 
+  TO_NUMBER = ENV['TO_NUMBER']
+end
 
 client = Nexmo::Client.new(
   api_key: NEXMO_API_KEY,
